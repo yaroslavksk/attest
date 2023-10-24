e28ed3bae29e0c41912c58c360aaef18af37894c
yaroslav_ksk <yaroslav_ksk@mail.ru>
2023-10-24 19:52:06 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.InteropServices;
+using System.Xml.Linq;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[,] a = { { 7, 3, 2 }, { 4, 9, 6 }, { 1, 8, 5 } };
+        int [] b = new int [9];
+        int count = 0;
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                Console.Write(a[i, j]);
+                b[count++] = a[i, j];
+            }
+        }
+        Array.Sort(b);
+        Console.WriteLine();
+        foreach (var i in b)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
